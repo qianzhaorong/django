0f5ec06efa4d23115fe454b40581d96561ed2b84
qianzhaorong <774151370@qq.com>
2019-05-03 15:55:44 +0800

    complete chapter3.3

--- student_sys/student/views.py
+++ student_sys/student/views.py
@@ -7,19 +7,11 @@ from .models import Student
 # Create your views here.
 
 def index(request):
-    students = Student.objects.all()
+    students = Student.get_all()
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            cleaned_data = form.cleaned_data
-            student = Student()
-            student.name = cleaned_data['name']
-            student.sex = cleaned_data['sex']
-            student.email = cleaned_data['email']
-            student.profession = cleaned_data['profession']
-            student.qq = cleaned_data['qq']
-            student.phone = cleaned_data['phone']
-            student.save()
+            form.save()
             return HttpResponseRedirect(reverse('index'))
     else:
         form = StudentForm()
