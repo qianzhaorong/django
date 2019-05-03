10022f7466088cb61a5a3339e3ce259492fd145f
qianzhaorong <774151370@qq.com>
2019-05-03 16:05:52 +0800

    使用class-based view替换函数式view的写法

--- student_sys/student/views.py
+++ student_sys/student/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import View
 
 from .forms import StudentForm
 from .models import Student
 # Create your views here.
 
-def index(request):
-    students = Student.get_all()
-    if request.method == 'POST':
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.get_all()
+        context = {
+            'students': students,
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('index'))
-    else:
-        form = StudentForm()
-    
-    context = {
-        'students': students,
-        'form': form,
-    }
-    return render(request, 'index.html', context=context) 
+        context = self.get_context()
+        context.update({
+            'form': form,
+        })
+        return render(request, self.template_name, context=context)
+
